PhoshMobi/gmobile

commit 9a8e0661c79c15618e5a4394a67ad0a3d64913c9

display-panel-run-phosh: Allow to take screenshot
Allow to take a screenshot via `-S name`. We use
a delayed callback to give phosh enough time to start
so we don't accidentally bind to another process exposing
the same API. Should that not prove to be robust enough
we can spawn a separate bus.

Closes: https://gitlab.gnome.org/World/Phosh/gmobile/-/issues/10
Part-of: <https://gitlab.gnome.org/World/Phosh/gmobile/-/merge_requests/90>

examples/gm-display-panel-run-phosh.c
@@ -24,6 +24,7 @@
 
 GMainLoop   *loop;
 GSubprocess *phoc;
+GDBusProxy  *proxy;
 
 G_NORETURN static void
 print_version (void)
@@ -33,8 +34,8 @@ print_version (void)
 }
 
 
-static gboolean
-on_shutdown_signal (gpointer unused)
+static void
+quit(void)
 {
   g_autoptr (GError) err = NULL;
   gboolean success;
@@ -46,6 +47,13 @@ on_shutdown_signal (gpointer unused)
     g_warning ("Failed to terminate phoc: %s", err->message);
 
   g_main_loop_quit (loop);
+}
+
+
+static gboolean
+on_shutdown_signal (gpointer unused)
+{
+  quit();
 
   return G_SOURCE_REMOVE;
 }
@@ -120,6 +128,60 @@ phoc_utils_compute_scale (int32_t phys_width, int32_t phys_height,
 }
 
 
+static void
+on_screenshot_proxy_ready (GObject *source_object, GAsyncResult *res, gpointer user_data)
+{
+  g_autoptr (GError) err = NULL;
+  g_autoptr (GVariant) result = NULL;
+  const char *filename, *template = user_data;
+  gboolean success;
+
+  g_assert (template);
+  proxy = g_dbus_proxy_new_for_bus_finish (res, &err);
+  if (!proxy) {
+    g_critical ("Failed to get screensaver proxy: %s", err->message);
+    if (!g_error_matches (err, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+      quit ();
+    return;
+  }
+
+  result = g_dbus_proxy_call_sync (proxy, "Screenshot",
+                                   g_variant_new ("(bbs)",
+                                                  FALSE,
+                                                  FALSE,
+                                                  template),
+                                   G_DBUS_CALL_FLAGS_NONE,
+                                   -1,
+                                   NULL,
+                                   &err);
+  if (!result) {
+    g_warning ("Failed to take screenshot: %s", err->message);
+  } else {
+    g_variant_get (result, "(b&s)", &success, &filename);
+    g_print ("Took screenshot '%s'", filename);
+  }
+
+  quit ();
+}
+
+
+static void
+on_timeout (gpointer user_data)
+{
+  char *template = user_data;
+
+  g_dbus_proxy_new_for_bus (G_BUS_TYPE_SESSION,
+                            G_DBUS_PROXY_FLAGS_NONE,
+                            NULL,
+                            "org.gnome.Shell.Screenshot",
+                            "/org/gnome/Shell/Screenshot",
+                            "org.gnome.Shell.Screenshot",
+                            NULL,
+                            on_screenshot_proxy_ready,
+                            template);
+}
+
+
 
 int
 main (int argc, char **argv)
@@ -131,6 +193,7 @@ main (int argc, char **argv)
   g_auto (GStrv) compatibles = NULL;
   GmDisplayPanel *panel = NULL;
   GStrv compatibles_opt = NULL;
+  char *screenshot_name = NULL;
   g_autofree char *phoc_ini = NULL;
   g_autoptr (GSubprocessLauncher) phoc_launcher = NULL;
   double scale_opt = -1.0;
@@ -143,6 +206,8 @@ main (int argc, char **argv)
      "The display scale", NULL },
     {"headless", 'H', 0, G_OPTION_ARG_NONE, &headless,
      "Use headless backend", NULL },
+    {"screenshot", 'S', 0, G_OPTION_ARG_FILENAME, &screenshot_name,
+     "Take screenshot", NULL},
     {"version", 0, 0, G_OPTION_ARG_NONE, &version,
      "Show version information", NULL},
     { NULL, 0, 0, G_OPTION_ARG_NONE, NULL, NULL, NULL }
@@ -211,6 +276,8 @@ main (int argc, char **argv)
                                        &err);
   g_unix_signal_add (SIGTERM, on_shutdown_signal, NULL);
   g_unix_signal_add (SIGINT, on_shutdown_signal, NULL);
+
+  g_timeout_add_seconds_once (2, on_timeout, screenshot_name);
 
   loop = g_main_loop_new (NULL, FALSE);
 

commit ceb1385165f77f7ce19c149d147dbe84c525a7d3

display-panel-run-phosh: Don't assert when not taking screenshots
Fixes: 9a8e066 ("display-panel-run-phosh: Allow to take screenshot")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/gmobile/-/merge_requests/93>

examples/gm-display-panel-run-phosh.c
@@ -207,7 +207,7 @@ main (int argc, char **argv)
     {"headless", 'H', 0, G_OPTION_ARG_NONE, &headless,
      "Use headless backend", NULL },
     {"screenshot", 'S', 0, G_OPTION_ARG_FILENAME, &screenshot_name,
-     "Take screenshot", NULL},
+     "Take screenshot with the given name", NULL},
     {"version", 0, 0, G_OPTION_ARG_NONE, &version,
      "Show version information", NULL},
     { NULL, 0, 0, G_OPTION_ARG_NONE, NULL, NULL, NULL }
@@ -277,7 +277,8 @@ main (int argc, char **argv)
   g_unix_signal_add (SIGTERM, on_shutdown_signal, NULL);
   g_unix_signal_add (SIGINT, on_shutdown_signal, NULL);
 
-  g_timeout_add_seconds_once (2, on_timeout, screenshot_name);
+  if (screenshot_name)
+    g_timeout_add_seconds_once (2, on_timeout, screenshot_name);
 
   loop = g_main_loop_new (NULL, FALSE);
 