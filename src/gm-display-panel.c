PhoshMobi/gmobile

commit 6ad76f439fb118aa90ed099fa41f2f449c476a61

Add Class describing a display panel

src/gm-display-panel.c
@@ -1,0 +1,380 @@
+/*
+ * Copyright (C) 2022 Guido Günther
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Guido Günther <agx@sigxcpu.org>
+ */
+
+#include "gm-cutout.h"
+#include "gm-display-panel.h"
+
+#include <json-glib/json-glib.h>
+
+/**
+ * GmDisplayPanel:
+ *
+ * Physical properties of a display panel like size, cutouts and
+ * rounded corners.
+ */
+
+enum {
+  PROP_0,
+  PROP_NAME,
+  PROP_CUTOUTS,
+  PROP_X_RES,
+  PROP_Y_RES,
+  PROP_WIDTH,
+  PROP_HEIGHT,
+  PROP_BORDER_RADIUS,
+  PROP_LAST_PROP
+};
+static GParamSpec *props[PROP_LAST_PROP];
+
+struct _GmDisplayPanel {
+  GObject     parent;
+
+  char       *name;
+  GListStore *cutouts;
+  int         x_res;
+  int         y_res;
+  int         border_radius;
+  int         width;
+  int         height;
+};
+
+static void gm_display_panel_json_serializable_iface_init (JsonSerializableIface *iface);
+
+G_DEFINE_TYPE_WITH_CODE (GmDisplayPanel, gm_display_panel, G_TYPE_OBJECT,
+                         G_IMPLEMENT_INTERFACE (JSON_TYPE_SERIALIZABLE,
+                                                gm_display_panel_json_serializable_iface_init));
+
+static void
+gm_display_panel_set_property (GObject      *object,
+                               guint         property_id,
+                               const GValue *value,
+                               GParamSpec   *pspec)
+{
+  GmDisplayPanel *self = GM_DISPLAY_PANEL (object);
+
+  switch (property_id) {
+  case PROP_NAME:
+    g_free (self->name);
+    self->name = g_value_dup_string (value);
+    break;
+  case PROP_CUTOUTS:
+    g_set_object (&self->cutouts, g_value_get_object (value));
+    break;
+  case PROP_X_RES:
+    self->x_res = g_value_get_int (value);
+    break;
+  case PROP_Y_RES:
+    self->y_res = g_value_get_int (value);
+    break;
+  case PROP_BORDER_RADIUS:
+    self->border_radius = g_value_get_int (value);
+    break;
+  case PROP_WIDTH:
+    self->width = g_value_get_int (value);
+    break;
+  case PROP_HEIGHT:
+    self->height = g_value_get_int (value);
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
+
+
+static void
+gm_display_panel_get_property (GObject    *object,
+                               guint       property_id,
+                               GValue     *value,
+                               GParamSpec *pspec)
+{
+  GmDisplayPanel *self = GM_DISPLAY_PANEL (object);
+
+  switch (property_id) {
+  case PROP_NAME:
+    g_value_set_string (value, self->name);
+    break;
+  case PROP_CUTOUTS:
+    g_value_set_object (value, self->cutouts);
+    break;
+  case PROP_X_RES:
+    g_value_set_int (value, gm_display_panel_get_x_res (self));
+    break;
+  case PROP_Y_RES:
+    g_value_set_int (value, gm_display_panel_get_y_res (self));
+    break;
+  case PROP_BORDER_RADIUS:
+    g_value_set_int (value, gm_display_panel_get_border_radius (self));
+    break;
+  case PROP_WIDTH:
+    g_value_set_int (value, gm_display_panel_get_width (self));
+    break;
+  case PROP_HEIGHT:
+    g_value_set_int (value, gm_display_panel_get_height (self));
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
+
+
+static JsonNode *
+gm_display_panel_serializable_serialize_property (JsonSerializable *serializable,
+                                                  const gchar      *property_name,
+                                                  const GValue     *value,
+                                                  GParamSpec       *pspec)
+{
+  GmDisplayPanel *self = GM_DISPLAY_PANEL (serializable);
+  JsonNode *node = NULL;
+
+  if (g_strcmp0 (property_name, "cutouts") == 0) {
+    JsonArray *array = json_array_sized_new (1);
+
+    for (int i = 0; i < g_list_model_get_n_items (G_LIST_MODEL (self->cutouts)); i++) {
+      g_autoptr (GObject) cutout = g_list_model_get_item (G_LIST_MODEL (self->cutouts), i);
+      json_array_add_element (array, json_gobject_serialize (cutout));
+    }
+    node = json_node_init_array (json_node_alloc (), array);
+  } else {
+    node = json_serializable_default_serialize_property (serializable,
+                                                         property_name,
+                                                         value,
+                                                         pspec);
+  }
+  return node;
+}
+
+
+static gboolean
+gm_display_panel_serializable_deserialize_property (JsonSerializable *serializable,
+                                                    const gchar      *property_name,
+                                                    GValue           *value,
+                                                    GParamSpec       *pspec,
+                                                    JsonNode         *property_node)
+{
+  if (g_strcmp0 (property_name, "cutouts") == 0) {
+    if (JSON_NODE_TYPE (property_node) == JSON_NODE_NULL) {
+      g_value_set_pointer (value, NULL);
+      return TRUE;
+    } else if (JSON_NODE_TYPE (property_node) == JSON_NODE_ARRAY) {
+      JsonArray *array = json_node_get_array (property_node);
+      guint array_len = json_array_get_length (array);
+      GListStore *cutouts = g_list_store_new (GM_TYPE_CUTOUT);
+
+      for (int i = 0; i < array_len; i++) {
+        JsonNode *element_node = json_array_get_element (array, i);
+        GmCutout *cutout;
+
+	if (JSON_NODE_HOLDS_OBJECT (element_node)) {
+	  cutout = GM_CUTOUT (json_gobject_deserialize (GM_TYPE_CUTOUT, element_node));
+          g_list_store_append (cutouts, cutout);
+	} else {
+	  return FALSE;
+	}
+      }
+      g_value_set_object (value, cutouts);
+      return TRUE;
+    }
+    return FALSE;
+  } else {
+    return json_serializable_default_deserialize_property (serializable,
+							   property_name,
+							   value,
+							   pspec,
+							   property_node);
+  }
+  return FALSE;
+}
+
+
+static void
+gm_display_panel_json_serializable_iface_init (JsonSerializableIface *iface)
+{
+  iface->serialize_property = gm_display_panel_serializable_serialize_property;
+  iface->deserialize_property = gm_display_panel_serializable_deserialize_property;
+}
+
+
+static void
+gm_display_panel_finalize (GObject *object)
+{
+  GmDisplayPanel *self = GM_DISPLAY_PANEL(object);
+
+  g_clear_object (&self->cutouts);
+  g_clear_pointer (&self->name, g_free);
+
+  G_OBJECT_CLASS (gm_display_panel_parent_class)->finalize (object);
+}
+
+
+static void
+gm_display_panel_class_init (GmDisplayPanelClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->get_property = gm_display_panel_get_property;
+  object_class->set_property = gm_display_panel_set_property;
+  object_class->finalize = gm_display_panel_finalize;
+
+  /**
+   * GmDisplayPanel:name:
+   *
+   * The name of the display
+   */
+  props[PROP_NAME] =
+    g_param_spec_string ("name", "", "",
+                         NULL,
+                         G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+
+  /**
+   * GmDisplayPanel:cutouts:
+   *
+   * The display cutouts as `GListModel` of [class@Cutout].
+   */
+  props[PROP_CUTOUTS] =
+    g_param_spec_object ("cutouts", "", "",
+                         G_TYPE_LIST_STORE,
+                         G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+  /**
+   * GmDisplayPanel:x-res:
+   *
+   * The panel resolution in pixels in the x direction
+   */
+  props[PROP_X_RES] =
+    g_param_spec_int ("x-res", "", "",
+                      0, G_MAXINT, 0,
+                      G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+  /**
+   * GmDisplayPanel:y-res:
+   *
+   * The panel resolution in pixels in the y direction
+   */
+  props[PROP_Y_RES] =
+    g_param_spec_int ("y-res", "", "",
+                      0, G_MAXINT, 0,
+                      G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+  /**
+   * GmDisplayPanel:border-radius:
+   *
+   * The border radius of the panel edges in device pixels
+   * If a single border radius isn't enough use multiple [type@Cutout].
+   */
+  props[PROP_BORDER_RADIUS] =
+    g_param_spec_int ("border-radius", "", "",
+                      0, G_MAXINT, 0,
+                      G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+  /**
+   * GmDisplayPanel:width:
+   *
+   * The display width in millimeters
+   */
+  props[PROP_WIDTH] =
+    g_param_spec_int ("width", "", "",
+                      0, G_MAXINT, 0,
+                      G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+  /**
+   * GmDisplayPanel:height:
+   *
+   * The display height in millimeters
+   */
+  props[PROP_HEIGHT] =
+    g_param_spec_int ("height", "", "",
+                      0, G_MAXINT, 0,
+                      G_PARAM_READWRITE | G_PARAM_STATIC_STRINGS);
+
+  g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
+}
+
+
+static void
+gm_display_panel_init (GmDisplayPanel *self)
+{
+  self->cutouts = g_list_store_new (GM_TYPE_CUTOUT);
+}
+
+
+GmDisplayPanel *
+gm_display_panel_new (void)
+{
+  return GM_DISPLAY_PANEL (g_object_new (GM_TYPE_DISPLAY_PANEL, NULL));
+}
+
+GmDisplayPanel *
+gm_display_panel_new_from_data (const gchar *data, GError **error)
+{
+  g_autoptr (JsonNode) node = json_from_string(data, error);
+  if (!node)
+    return NULL;
+
+  return GM_DISPLAY_PANEL (json_gobject_deserialize (GM_TYPE_DISPLAY_PANEL, node));
+}
+
+const char *
+gm_display_panel_get_name (GmDisplayPanel *self)
+{
+  g_return_val_if_fail (GM_IS_DISPLAY_PANEL (self), NULL);
+
+  return self->name;
+}
+
+/**
+ * gm_display_panel_get_cutouts:
+ * @self: The display panel
+ *
+ * Get the display cutouts.
+ *
+ * Returns: (transfer none): The display cutouts
+ */
+GListModel *
+gm_display_panel_get_cutouts (GmDisplayPanel *self)
+{
+  g_return_val_if_fail (GM_IS_DISPLAY_PANEL (self), NULL);
+
+  return G_LIST_MODEL (self->cutouts);
+}
+
+int
+gm_display_panel_get_x_res (GmDisplayPanel *self)
+{
+  g_return_val_if_fail (GM_IS_DISPLAY_PANEL (self), 0);
+
+  return self->x_res;
+}
+
+int
+gm_display_panel_get_y_res (GmDisplayPanel *self)
+{
+  g_return_val_if_fail (GM_IS_DISPLAY_PANEL (self), 0);
+
+  return self->y_res;
+}
+
+int
+gm_display_panel_get_border_radius (GmDisplayPanel *self)
+{
+  g_return_val_if_fail (GM_IS_DISPLAY_PANEL (self), 0);
+
+  return self->border_radius;
+}
+
+int
+gm_display_panel_get_width (GmDisplayPanel *self)
+{
+  g_return_val_if_fail (GM_IS_DISPLAY_PANEL (self), 0);
+
+  return self->width;
+}
+
+int
+gm_display_panel_get_height (GmDisplayPanel *self)
+{
+  g_return_val_if_fail (GM_IS_DISPLAY_PANEL (self), 0);
+
+  return self->height;
+}

meson.build
@@ -60,6 +60,7 @@ endif
 
 glib_dep = dependency('glib-2.0', version: '>=2.66')
 gio_dep = dependency('gio-2.0', version: '>=2.66')
+json_glib_dep = dependency('json-glib-1.0')
 
 foreach arg: test_c_args
   if cc.has_multi_arguments(arg)

src/gm-display-panel.h
@@ -1,0 +1,28 @@
+/*
+ * Copyright (C) 2022 Guido Günther
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <glib-object.h>
+#include <gio/gio.h>
+
+G_BEGIN_DECLS
+
+#define GM_TYPE_DISPLAY_PANEL (gm_display_panel_get_type ())
+
+G_DECLARE_FINAL_TYPE (GmDisplayPanel, gm_display_panel, GM, DISPLAY_PANEL, GObject)
+
+GmDisplayPanel     *gm_display_panel_new (void);
+GmDisplayPanel     *gm_display_panel_new_from_data (const gchar *data, GError **error);
+const char         *gm_display_panel_get_name (GmDisplayPanel *self);
+GListModel         *gm_display_panel_get_cutouts (GmDisplayPanel *self);
+int                 gm_display_panel_get_x_res (GmDisplayPanel *self);
+int                 gm_display_panel_get_y_res (GmDisplayPanel *self);
+int                 gm_display_panel_get_border_radius (GmDisplayPanel *self);
+int                 gm_display_panel_get_width (GmDisplayPanel *self);
+int                 gm_display_panel_get_height (GmDisplayPanel *self);
+
+G_END_DECLS

src/gmobile.h
@@ -17,5 +17,6 @@
 #include "gm-config.h"
 #include "gm-cutout.h"
 #include "gm-device-tree.h"
+#include "gm-display-panel.h"
 #include "gm-error.h"
 #include "gm-timeout.h"

src/meson.build
@@ -1,6 +1,7 @@
 gm_deps = [
   gio_dep,
   glib_dep,
+  json_glib_dep,
   cc.find_library('m', required: false),
   cc.find_library('rt', required: false),
 ]
@@ -11,6 +12,8 @@ gm_sources = files(
   'gm-cutout.h',
   'gm-device-tree.c',
   'gm-device-tree.h',
+  'gm-display-panel.c',
+  'gm-display-panel.h',
   'gm-error.c',
   'gm-error.h',
   'gm-rect.c',

tests/meson.build
@@ -12,6 +12,7 @@ test_cflags = [
 
 tests = [
   'device-tree',
+  'display-panel',
   'cutout',
   'svg-path',
   'timeout',

tests/test-display-panel.c
@@ -1,0 +1,69 @@
+/*
+ * Copyright (C) 2022 Guido Günther <agx@sigxcpu.org>
+ *
+ * SPDX-License-Identifier: GPL-3-or-later
+ */
+
+#define GMOBILE_USE_UNSTABLE_API
+#include "gmobile.h"
+
+
+static void
+test_gm_display_panel_parse (void)
+{
+  const char *json = "                                "
+    "{                                                "
+    " \"name\": \"Oneplus 6T\",                       "
+    " \"x-res\": 1080,                                "
+    " \"y-res\": 2340,                                "
+    " \"border-radius\": 10,                          "
+    " \"width\": 68,                                  "
+    " \"height\": 145,                                "
+    " \"cutouts\" : [                                 "
+    "     {                                           "
+    "        \"name\": \"notch\",                     "
+    "        \"path\": \"M 455 0 V 79 H 625 V 0 Z\"   "
+    "     }                                           "
+    "  ]                                              "
+    "}                                                ";
+  g_autoptr (GError) err = NULL;
+  g_autoptr (GmDisplayPanel) panel = NULL;
+  g_autoptr (GmCutout) cutout = NULL;
+  GListModel *cutouts;
+  const GmRect *bounds;
+
+  panel = gm_display_panel_new_from_data (json, &err);
+  g_assert_no_error (err);
+  g_assert_nonnull (panel);
+
+  cutouts = gm_display_panel_get_cutouts (panel);
+  g_assert_cmpint (g_list_model_get_n_items (cutouts), ==, 1);
+  cutout = g_list_model_get_item (cutouts, 0);
+  g_assert_nonnull (cutout);
+  g_assert_cmpstr (gm_cutout_get_name (cutout), ==, "notch");
+  g_assert_cmpstr (gm_cutout_get_path (cutout), ==, "M 455 0 V 79 H 625 V 0 Z");
+  bounds = gm_cutout_get_bounds (cutout);
+  g_assert_cmpint (bounds->x, ==, 455);
+  g_assert_cmpint (bounds->y, ==, 0);
+  g_assert_cmpint (bounds->width, ==, 170);
+  g_assert_cmpint (bounds->height, ==, 79);
+
+  g_assert_cmpint (gm_display_panel_get_x_res (panel), ==, 1080);
+  g_assert_cmpint (gm_display_panel_get_y_res (panel), ==, 2340);
+
+  g_assert_cmpint (gm_display_panel_get_border_radius (panel), ==, 10);
+
+  g_assert_cmpint (gm_display_panel_get_width (panel), ==, 68);
+  g_assert_cmpint (gm_display_panel_get_height (panel), ==, 145);
+}
+
+
+gint
+main (gint argc, gchar *argv[])
+{
+  g_test_init (&argc, &argv, NULL);
+
+  g_test_add_func ("/Gm/display-panel/parse", test_gm_display_panel_parse);
+
+  return g_test_run ();
+}

commit 42139fc3f6e391d31456ecb686f448446e58a663

gm-display-panel: Untabify
Fixes: 6ad76f4 ("Add Class describing a display panel")
Gbp-Dch: Ignore
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/gmobile/-/merge_requests/93>

src/gm-display-panel.c
@@ -174,12 +174,12 @@ gm_display_panel_serializable_deserialize_property (JsonSerializable *serializab
         JsonNode *element_node = json_array_get_element (array, i);
         g_autoptr (GmCutout) cutout = NULL;
 
-	if (JSON_NODE_HOLDS_OBJECT (element_node)) {
-	  cutout = GM_CUTOUT (json_gobject_deserialize (GM_TYPE_CUTOUT, element_node));
+        if (JSON_NODE_HOLDS_OBJECT (element_node)) {
+          cutout = GM_CUTOUT (json_gobject_deserialize (GM_TYPE_CUTOUT, element_node));
           g_list_store_append (cutouts, cutout);
-	} else {
-	  return FALSE;
-	}
+        } else {
+          return FALSE;
+        }
       }
       g_value_set_object (value, cutouts);
       return TRUE;
@@ -187,10 +187,10 @@ gm_display_panel_serializable_deserialize_property (JsonSerializable *serializab
     return FALSE;
   } else {
     return json_serializable_default_deserialize_property (serializable,
-							   property_name,
-							   value,
-							   pspec,
-							   property_node);
+                                                           property_name,
+                                                           value,
+                                                           pspec,
+                                                           property_node);
   }
   return FALSE;
 }